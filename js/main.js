54fcef231943cfad0621be11ee56960954a13218
TatyanaTropkina <tropkina.tatyana@gmail.com>
2021-06-27 15:44:02 +0300

    add animation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-
-// $(function () {
-//     $(".header__content, .footer__inner").on("click", "a", function (event) {
-//         event.preventDefault();
-//         var id = $(this).attr('href'),
-//             top = $(id).offset().top;
-//         $('body,html').animate({
-//             scrollTop: top
-//         }, 1500);
-//     });
-// });
-
+document.body.addEventListener('touchstart', function(e){ e.preventDefault(); });
+document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
 $(function(){
+	$(document).ready(function(){
+		$("#header ,  #footer").on("click","a", function (event) {
+			event.preventDefault();
+			let id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1500);
+		});
+	});
 	$(document).ready(function () {
 		let preloader = $('.preloader'),
 loader = preloader.find('.preloader__wrapper');
@@ -21,7 +19,10 @@ loader.fadeOut();
 });
 
 	$(document).ready(function() {
-		$('.header__title , .header__subtitle , header__btn-scroll, spinner').addClass('loaded');
+		$('.header__title').addClass('animate__animated animate__fadeInDown');
+		$('.header__subtitle').addClass('animate__animated animate__fadeInDown animate__delay-1s');
+		$('.header__btn-scroll').addClass('animate__animated animate__fadeInUp animate__delay-1s')
+
 	});
 	$('.portfolio__items-more').hide();
 
@@ -50,6 +51,13 @@ loader.fadeOut();
             $('.menu__list').removeClass('active');
         }
     });
+	
+	$(window).scroll(function(){
+		if($(this).scrollTop()> $('.subscription__btn').offset().top - 200) {
+			$('.logo').addClass('anim');	
+		}
+	});
+	
 	$('body').click(function(event) {
 		$target = $(event.target);
 		if (!$target.closest($('.menu__btn , .menu__list')).length){
@@ -61,14 +69,8 @@ loader.fadeOut();
 $('.about__title , .expertise__title , .portfolio__title , .team__title , .pricing__title , .subscription__title').addClass('animate__animated animate__fadeInLeft wow');
 $('about__title , .expertise__title , .portfolio__title , .team__title , .pricing__title , .subscription__title').attr('data-wow-offset', '200');
 
-
-$('.about__content , .expertise__inner , .expertise__cards , .features__content , .portfolio__inner , .team__items , .pricing__cards , .subscription__btn ').addClass('animate__animated animate__fadeInUp wow');
+$('.about__content , .expertise__inner , .expertise__cards , .features__content , .portfolio__inner , .team__items , .pricing__cards ').addClass('animate__animated animate__fadeInUp wow');
 $('.about__content , .expertise__inner , .expertise__cards , .features__content , .portfolio__inner , .team__items , .pricing__cards ').attr('data-wow-offset', '240');
-
-
-	
-	
-	
 	$('.gallery-slider').slick({
 		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
 		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
@@ -83,7 +85,6 @@ $('.about__content , .expertise__inner , .expertise__cards , .features__content 
 			                   arrows: false
 			               }
 			   },
-			
 			 ]
 	});
 	var mixer = mixitup('.portfolio__inner', {
@@ -98,17 +99,8 @@ $('.about__content , .expertise__inner , .expertise__cards , .features__content 
 		}
 		
 	});
-	$(window).scroll(function(){
-	
-		if($(this).scrollTop()> $('.subscription').offset().top - 100) {
-			$('.logo').addClass('anim');
-			
-		}
-		
-	});
+
 });
-
-
 $(function(){
 
 	let video = document.getElementById('presentation-video');
@@ -132,12 +124,5 @@ $(function(){
 	})
 
 });
-wow = new WOW(
-	{
-	// mobile:       false
-  }
-  )
+wow = new WOW()
   wow.init();
-
-  document.body.addEventListener('touchstart', function(e){ e.preventDefault(); });
-  document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
