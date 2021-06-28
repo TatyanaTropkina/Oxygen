1578e0b2d801994e2c4592f273a02996f0ae6413
TatyanaTropkina <tropkina.tatyana@gmail.com>
2021-06-28 10:18:11 +0300

    fix animate

--- js/main.js
+++ js/main.js
@@ -104,10 +104,10 @@ $(function(){
 					  return false;
 		}
 	  });
-	  $('.about__title , .expertise__title , .portfolio__title , .team__title , .pricing__title , .subscription__title').addClass('animate__animated animate__fadeInLeft wow');
+	  $('.about__title , .expertise__title , .portfolio__title , .team__title , .pricing__title , .subscription__title').addClass('animate__ animate__fadeInLeft wow animated');
 $('about__title , .expertise__title , .portfolio__title , .team__title , .pricing__title , .subscription__title').attr('data-wow-offset', '200');
 
-$('.about__content , .expertise__inner , .expertise__cards , .features__content , .portfolio__inner , .team__items , .pricing__cards ').addClass('animate__animated animate__fadeInUp wow');
+$('.about__content , .expertise__inner , .expertise__cards , .features__content , .portfolio__inner , .team__items , .pricing__cards ').addClass('animate__ animate__fadeInUp wow animated');
 $('.about__content , .expertise__inner , .expertise__cards , .features__content , .portfolio__inner , .team__items , .pricing__cards ').attr('data-wow-offset', '240');
 });
 $(function(){
