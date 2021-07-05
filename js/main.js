45699a6e41f0b5f35d895c73662c7c4b5f2fcab0
TatyanaTropkina <tropkina.tatyana@gmail.com>
2021-07-05 22:14:46 +0300

    update js

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ $(function(){
 	  });
 	
 		$('.about__title , .expertise__title , .portfolio__title , .team__title , .pricing__title , .subscription__title').addClass('animate__animated animate__fadeInLeft wow');
-		$('about__title , .expertise__title , .portfolio__title , .team__title , .pricing__title , .subscription__title').attr('data-wow-offset', '200');
+		$('.about__title , .expertise__title , .portfolio__title , .team__title , .pricing__title , .subscription__title').attr('data-wow-offset', '200');
 		
 		$('.about__content , .expertise__inner , .expertise__cards , .features__content , .portfolio__inner , .team__items , .pricing__cards ').addClass('animate__animated animate__fadeInUp wow');
 		$('.about__content , .expertise__inner , .expertise__cards , .features__content , .portfolio__inner , .team__items , .pricing__cards ').attr('data-wow-offset', '240');
